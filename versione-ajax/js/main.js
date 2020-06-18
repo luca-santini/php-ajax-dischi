4df15b08479a2ce6f5482ec9e2ca2b00d9e33343
Luca Santini <lucasantinidev@gmail.com>
2020-06-18 16:53:14 +0200

    versione ajax handlebars template

--- versione-ajax/js/main.js
+++ versione-ajax/js/main.js
@@ -1,31 +1,32 @@
 $(document).ready(function() {
-
+    // recupero la struttura html del template di base
+    var template_html = $('#template-dischi').html();
+    // preparo la funzione da utilizzare per utilizzare il template
+    var template_function = Handlebars.compile(template_html);
 
     $.ajax ({
         'url': 'database/lista_dischi.php',
         'method': 'GET',
         'success': function(data) {
-            var dischi = data.response;
-            // recupero la struttura html del template di base
-            var template_html = $('#template-dischi').html();
-            // preparo la funzione da utilizzare per utilizzare il template
-            var template_function = Handlebars.compile(template_html);
+            var dischi = data;
+
 
             for (var i = 0; i < dischi.length; i++) {
-                var disco = dischi[i];
-                console.log(disco);
-                var dati_disco = {
-                    'immagine': disco.poster,
-                    'titolo': disco.title,
-                    'autore': disco.author,
-                    'genere': disco.genre,
-                    'anno': disco.year
-                }
-                // tramite handlebars preparo l'html finale con i dati dei dischi
-                var html_finale = template_function(dati_disco);
-
-                // appendo in pagina i dati dei dischi
-                $('.cds-container').append(html_finale);
+                disegna_card(dischi[i])
+                // var disco = dischi[i];
+                // console.log(disco);
+                // var dati_disco = {
+                //     'immagine': disco.poster,
+                //     'titolo': disco.title,
+                //     'autore': disco.author,
+                //     'genere': disco.genre,
+                //     'anno': disco.year
+                // }
+                // // tramite handlebars preparo l'html finale con i dati dei dischi
+                // var html_finale = template_function(dati_disco);
+                //
+                // // appendo in pagina i dati dei dischi
+                // $('.cds-container').append(html_finale);
             }
         },
         'error': function() {
@@ -42,14 +43,14 @@ $(document).ready(function() {
 
         // preparo i placeholder per il template
         var context = {
-            'copertina_album': immagine,
-            'titolo_album': titolo,
-            'autore_album': autore,
-            'anno_album': anno,
-            'genere_album': genere
+            'immagine': immagine,
+            'titolo': titolo,
+            'autore': autore,
+            'anno': anno,
+            'genere': genere
         };
         // compilo il template con i placeholder
-        var html = template(context);
+        var html = template_function(context);
         // appendo la card per questo disco
         $('.cds-container').append(html);
     }
